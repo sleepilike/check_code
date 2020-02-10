99b3a612a2c20a44cc8e8b11ea29c1e40442004f
sleepilike <980683046@qq.com>
2020-02-10 20:49:54 +0800

    添加接触者健康情况列

--- lib/ui/mainwidget/person.dart
+++ lib/ui/mainwidget/person.dart
@@ -8,8 +8,9 @@ class Person {
   final String tel;
   final String address;
   final String health;
+  final String others;
   Person(
-      this.name,this.age,this.sex,this.tel,this.address,this.health
+      this.name,this.age,this.sex,this.tel,this.address,this.health,this.others
       );
 }
 
@@ -22,23 +23,23 @@ class PersonListView extends StatefulWidget{
 
 class _PersonListViewState extends State<PersonListView>{
   final  persons=[
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
-    Person("胖虎","10","男","12345678912","广东广州","均无"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
+    Person("胖虎","10","男","12345678912","广东广州","均无","正常"),
   ];
   Widget build (BuildContext context){
     return SizedBox(
-      height: 240,
+      height: 200,
       child: ListView.builder(
           itemCount: persons.length,
           itemBuilder: (context,index){
@@ -74,24 +75,26 @@ class PersonPrint extends StatelessWidget{
         defaultVerticalAlignment: TableCellVerticalAlignment.middle,
         border: new TableBorder.all(color: Colors.white),
         columnWidths: const<int,TableColumnWidth>{
-          0:FixedColumnWidth(30.0),
-          1:FixedColumnWidth(60.0),
-          2:FixedColumnWidth(30.0),
-          3:FixedColumnWidth(30.0),
-          4:FixedColumnWidth(90.0),
-          5:FixedColumnWidth(70.0),
-          6:FixedColumnWidth(70.0),
+          //0:FixedColumnWidth(30.0),
+          0:FixedColumnWidth(60.0),
+          //2:FixedColumnWidth(30.0),
+          //3:FixedColumnWidth(30.0),
+          //4:FixedColumnWidth(90.0),
+          1:FixedColumnWidth(70.0),
+          2:FixedColumnWidth(70.0),
+          3:FixedColumnWidth(110.0),
         },
         children: <TableRow>[
           new TableRow(
             children: <Widget>[
-              new TableCell(child: buildTableCell(snum)),
+              //new TableCell(child: buildTableCell(snum)),
               new TableCell(child: buildTableCell(aperson.name)),
-              new TableCell(child: buildTableCell(aperson.age)),
-              new TableCell(child: buildTableCell(aperson.sex)),
-              new TableCell(child: buildTableCell(aperson.tel)),
+              //new TableCell(child: buildTableCell(aperson.age)),
+              //new TableCell(child: buildTableCell(aperson.sex)),
+              //new TableCell(child: buildTableCell(aperson.tel)),
               new TableCell(child: buildTableCell(aperson.address)),
               new TableCell(child: buildTableCell(aperson.health)),
+              new TableCell(child: buildTableCell(aperson.others)),
             ],
           ),
         ],
--- lib/ui/mainwidget/persons.dart
+++ lib/ui/mainwidget/persons.dart
@@ -27,7 +27,7 @@ class _PersonsState extends State<Persons>{
       CardItem(
           title: "监控信息",
           child: SizedBox(
-            height: 280,
+            height: 250,
             width: 200,
             child:Column(
               children: <Widget>[
@@ -40,34 +40,34 @@ class _PersonsState extends State<Persons>{
                   child:Table(
                     defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                     columnWidths: const<int,TableColumnWidth>{
-                      0:FixedColumnWidth(30.0),
-                      1:FixedColumnWidth(60.0),
-                      2:FixedColumnWidth(30.0),
-                      3:FixedColumnWidth(30.0),
-                      4:FixedColumnWidth(90.0),
-                      5:FixedColumnWidth(70.0),
-                      6:FixedColumnWidth(70.0),
+                      //0:FixedColumnWidth(30.0),
+                      0:FixedColumnWidth(60.0),
+                      //2:FixedColumnWidth(30.0),
+                      //3:FixedColumnWidth(30.0),
+                      //4:FixedColumnWidth(90.0),
+                      1:FixedColumnWidth(70.0),
+                      2:FixedColumnWidth(70.0),
+                      3:FixedColumnWidth(110.0),
                     },
                     border: TableBorder.all(color: Colors.white),
                     children: <TableRow>[
                       new TableRow(
                         children: <Widget>[
-                          new TableCell(child: buildTableCell("序号")),
+                         // new TableCell(child: buildTableCell("序号")),
                           new TableCell(child: buildTableCell("姓名")),
-                          new TableCell(child: buildTableCell("年龄")),
-                          new TableCell(child: buildTableCell("性别")),
-                          new TableCell(child: buildTableCell("手机号码")),
+                          //new TableCell(child: buildTableCell("年龄")),
+                          //new TableCell(child: buildTableCell("性别")),
+                          //new TableCell(child: buildTableCell("手机号码")),
                           new TableCell(child: buildTableCell("所处地点 ")),
                           new TableCell(child: buildTableCell("健康情况")),
+                          new TableCell(child: buildTableCell("接触者健康情况")),
                         ],
                       ),
                     ],
                   ) ,
                 ),
-                Expanded(
-                  flex: 1,
-                  child: PersonListView(),
-                )
+                 PersonListView(),
+
 
               ],
             ) ,)
